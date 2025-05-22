62db312b9cc5a39eef59895dbe3933d2e505f801
chijason99 <chijason99@gmail.com>
2025-05-22 18:40:07 +0100

    feat: initial migration + project folder name change

---
+++ ProductManagementCatalogue.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementCatalogue.Application.Dtos.Products;
+using ProductManagementCatalogue.Application.Services;
+using ProductManagementCatalogue.Domain.Shared.Results;
+
+namespace ProductManagementCatalogue.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController(IProductService productService) : ControllerBase
+{
+	private readonly IProductService _productService = productService;
+
+	[HttpPost]
+	[ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ProductDto))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(IEnumerable<string>))]
+	public async Task<IActionResult> CreateAsync(CreateProductDto productDto, CancellationToken cancellationToken = default)
+	{
+		Result<ProductDto> createProductResult = await _productService.CreateProductAsync(productDto, cancellationToken);
+
+		if (createProductResult.IsFailure)
+			return BadRequest();
+
+		return Created();
+	}
+}
